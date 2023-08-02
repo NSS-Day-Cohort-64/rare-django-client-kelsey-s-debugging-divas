c77fa7a1bd4a447244d12f3971a9ac8138d49aaa
Casey Ferebee <cwferebee@gamil.com>
2023-08-02 10:03:08 -0500

    commit to pull from main

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -7,3 +7,20 @@ export const getSinglePost = (postId) => {
   return fetch(`http://localhost:8088/posts/${postId}`)
     .then(res => res.json())
 }
+
+export const createPost = (newPost) => {
+  return fetch("http://localhost:8088/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to create post.");
+      }
+    });
+};
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,30 +1,27 @@
-import { Route, Routes } from "react-router-dom"
-import { Login } from "../components/auth/Login"
-import { Register } from "../components/auth/Register"
-import { Authorized } from "./Authorized"
-import { CategoryForm } from "../components/categories/CategoryForm"
-import { PostList } from "../components/posts/PostsList"
-import PostDetails from "../components/posts/PostDetails"
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/auth/Login";
+import { Register } from "../components/auth/Register";
+import { Authorized } from "./Authorized";
+import { CategoryForm } from "../components/categories/CategoryForm";
+import { PostList } from "../components/posts/PostsList";
+import PostDetails from "../components/posts/PostDetails";
+import { PostForm } from "../components/posts/PostForm";
 
 export const ApplicationViews = ({ token, setToken }) => {
-  return <>
+  return (
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />} />
       <Route path="/register" element={<Register setToken={setToken} />} />
-      <Route element={<Authorized token={token} />} />
-
-      <Route path="/posts">
+      <Route element={<Authorized token={token} />}>
+        {/* Nested Routes for Posts */}
         <Route index element={<PostList setToken={setToken} />} />
         <Route path=":postId" element={<PostDetails setToken={setToken} />} />
-        <Route path="/posts/create" element={<PostForm setToken={setToken} />} />
+        <Route path="create" element={<PostForm setToken={setToken} />} />
       </Route>
-  
-      <Route path="/categories">
-        <Route path='create' element={<CategoryForm setToken={setToken} />}  />
-  
-      </Route>
+
+      
+      <Route path="/categories" element={<CategoryForm setToken={setToken} />} />
     </Routes>
-    
-  </>
-}
+  );
+};

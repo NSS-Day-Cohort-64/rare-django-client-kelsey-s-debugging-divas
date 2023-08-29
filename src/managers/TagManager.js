6f445f50248b6ded332da0158cf5acd4d8ec893d
Randy Hamm <randykhamm@gmail.com>
2023-08-29 10:14:28 -0500

    edited getTags fetch call with the new port

--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -1,15 +1,19 @@
-export const createTag = (newTag) => {
+export const createTag = (newTag, token) => {
   return fetch("http://localhost:8000/tags", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json"
+      "Accept": "application/json",
+      "Authorization": `Token ${localStorage.getItem("auth_token")}`
     },
     body: JSON.stringify(newTag),
-  })
+  });
 };
 
-export const getAllTags = () => {
-  return fetch("http://localhost:8000/tags")
-    .then(response => response.json())
+export const getAllTags = (token) => {
+  return fetch("http://localhost:8000/tags", {
+    headers: {
+      "Authorization": `Token ${localStorage.getItem("auth_token")}`
+    }
+  }).then(response => response.json());
 };
